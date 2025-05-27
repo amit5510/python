4f4df130127c6f20c244a1143a1efaab58eae596
upchar <42610901+amit5510@users.noreply.github.com>
2025-05-28 00:07:39 +0530

    Update table.py

--- table.py
+++ table.py
@@ -1,7 +1,7 @@
 # Program to print multiplication table of a number
 
 # Ask user to enter a number
-num = 5
+num = 51
 
 # Print the table from 1 to 10
 print(f"\nMultiplication Table of {num}:\n")
